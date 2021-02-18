bf581246435c5ac468c2c057b21b5caf10f8f776
Szymon Olewniczak <solewniczak@rid.pl>
2021-02-18 09:26:50 +0100

    pluginwiz

---
+++ helper/update.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DokuWiki Plugin structupdate (Helper Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Szymon Olewniczak <it@rid.pl>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) {
+    die();
+}
+
+class helper_plugin_structupdate_update extends DokuWiki_Plugin
+{
+
+}
+
